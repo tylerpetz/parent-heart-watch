44ee39be724095211e49b2d3b92bfeabce87675e
Tyler Petz <tylerpetz@gmail.com>
2016-08-17 00:07:33 -0400

    almost finished homepage

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,5 +1,22 @@
 $(document).foundation();
 
+$(document).ready(function() {
+  $("#slider").owlCarousel({
+  	items: 1,
+  	singleItem: true,
+  	touchDrag: false,
+  	mouseDrag: false
+  });
+
+  $(".news-slider").owlCarousel({
+  	items: 3,
+  	touchDrag: false,
+  	mouseDrag: false,
+  	pagination: false,
+  	navigation: true
+  });
+});
+
 $(document).on('click touchstart', '.menu-icon', function () {
 	if ($('nav').hasClass('open')) {
 
