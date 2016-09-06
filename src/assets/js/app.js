20b3b80224cc08d2ae3ed34e3df395a5d8f6f531
Tyler Petz <tylerpetz@gmail.com>
2016-09-05 20:47:54 -0400

    added events, legislation, conference, home page popup, responsive is all that is left

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,6 +1,24 @@
 $(document).foundation();
 
 $(document).ready(function() {
+
+  var url = window.location.pathname;
+  url = url.replace('/', '');
+  $('header nav a').each(function() {
+    if ($(this).attr('href') == url) {
+      $(this).parent().addClass("active");
+      $(this).parents('.dropdown').addClass("active");
+    }
+  });
+
+  $('.dropdown').mouseover(function () {
+    $('.dropdown').not($(this)).find('ul').hide();
+    $(this).find('ul').show();
+  }).mouseout(function () {
+    $('.dropdown').find('ul').hide();
+    $('.dropdown.active').find('ul').show();
+  });
+
   $("#slider").owlCarousel({
   	items: 1,
   	singleItem: true,
@@ -33,3 +51,5 @@ $(document).on('click touchstart', '.menu-icon', function () {
 		$('nav').addClass('open pop-in').removeClass('pop-out');
 	}
 });
+
+
